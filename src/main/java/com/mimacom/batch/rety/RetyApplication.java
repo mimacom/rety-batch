e11d13775386e98115342d68658ce39642f32d45
Sergej Kunz <sergej.kunz@mimacom.com>
2018-07-20 14:29:11 +0200

    init

---
+++ src/main/java/com/mimacom/batch/rety/RetyApplication.java
@@ -0,0 +1,12 @@
+package com.mimacom.batch.rety;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetyApplication.class, args);
+	}
+}
